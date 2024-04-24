39dfb7fccf16826fb5fe56873525a4b85d006834
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2024-04-24 11:54:14 +0200

    another step

--- chat-application/src/main/java/io/quarkiverse/langchain4j/workshop/chat/ChatService.java
+++ chat-application/src/main/java/io/quarkiverse/langchain4j/workshop/chat/ChatService.java
@@ -10,13 +10,15 @@ import jakarta.inject.Singleton;
 @Singleton
 public interface ChatService {
 
+    @SystemMessage("""
+         <<SYS>>You are a chat bot answering to customer request about bank products.<</SYS>>
+        """)
     @UserMessage("""
-        You are a chat bot answering to customer request about bank products.
         Answer the customer request. The answer must be polite, and be relevant to the question.
         When you don't know, respond that you don't know the answer and the bank will contact the customer directly.
-
-
+        +++
         {message}
+        +++
         """)
     String chat(@MemoryId Object session, String message);
 
--- chat-application/src/main/java/io/quarkiverse/langchain4j/workshop/chat/DocumentRetriever.java
+++ chat-application/src/main/java/io/quarkiverse/langchain4j/workshop/chat/DocumentRetriever.java
@@ -6,7 +6,7 @@ import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
-import io.quarkiverse.langchain4j.infinispan.InfinispanEmbeddingStore;
+import io.quarkiverse.langchain4j.redis.RedisEmbeddingStore;
 import jakarta.enterprise.context.ApplicationScoped;
 
 
@@ -17,7 +17,7 @@ public class DocumentRetriever implements Supplier<RetrievalAugmentor> {
 
     private final RetrievalAugmentor augmentor;
 
-    DocumentRetriever(InfinispanEmbeddingStore store, AllMiniLmL6V2EmbeddingModel model) {
+    DocumentRetriever(RedisEmbeddingStore store, AllMiniLmL6V2EmbeddingModel model) {
         EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
             .embeddingModel(model)
             .embeddingStore(store)
