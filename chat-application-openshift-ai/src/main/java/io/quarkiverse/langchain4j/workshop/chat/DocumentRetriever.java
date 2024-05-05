97daa88d006ffd4a71bbea78ae53c38cdfd3497d
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2024-05-05 16:53:13 +0200

    Finished now it's time to try it out

--- chat-application-openshift-ai/src/main/java/io/quarkiverse/langchain4j/workshop/chat/DocumentRetriever.java
+++ chat-application-openshift-ai/src/main/java/io/quarkiverse/langchain4j/workshop/chat/DocumentRetriever.java
@@ -1,25 +1,37 @@
 package io.quarkiverse.langchain4j.workshop.chat;
 
-import dev.langchain4j.data.segment.TextSegment;
+
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.retriever.EmbeddingStoreRetriever;
-import dev.langchain4j.retriever.Retriever;
-import io.quarkiverse.langchain4j.redis.RedisEmbeddingStore;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import io.quarkiverse.langchain4j.infinispan.InfinispanEmbeddingStore;
 import jakarta.enterprise.context.ApplicationScoped;
 
-import java.util.List;
+
+import java.util.function.Supplier;
 
 @ApplicationScoped
-public class DocumentRetriever implements Retriever<TextSegment> {
+public class DocumentRetriever implements Supplier<RetrievalAugmentor> {
 
-    private final EmbeddingStoreRetriever retriever;
+    private final RetrievalAugmentor augmentor;
 
-    DocumentRetriever(RedisEmbeddingStore store, EmbeddingModel model) {
-        retriever = EmbeddingStoreRetriever.from(store, model, 4);
+    DocumentRetriever(InfinispanEmbeddingStore store, EmbeddingModel model) {
+        EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+            .embeddingModel(model)
+            .embeddingStore(store)
+            .maxResults(3)
+            .build();
+
+        augmentor = DefaultRetrievalAugmentor
+            .builder()
+            .contentRetriever(contentRetriever)
+            .build();
     }
 
     @Override
-    public List<TextSegment> findRelevant(String s) {
-        return retriever.findRelevant(s);
+    public RetrievalAugmentor get() {
+        return augmentor;
     }
+
 }
